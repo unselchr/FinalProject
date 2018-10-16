12f0cba10ba49ee08e6a47c5912c19bf283ac1f3
unselchr <cdojrf@gmail.com>
2018-10-15 19:23:07 -0500

    add hq is kinda working

---
+++ client/src/utils/units.js
@@ -0,0 +1,30 @@
+const units={
+  hq:[
+    {name:"Lord Castellan Creed",points:70,powerLevel:4}
+  ],
+  troops:[
+
+  ],
+  elites:[
+
+  ],
+  fastAttack:[
+
+  ],
+  heavySupport:[
+
+  ],
+  lordOfWar:[
+
+  ],
+  transport:[
+
+  ],
+  flyer:[
+
+  ],
+  fortification:[
+
+  ]
+}
+export default units;
