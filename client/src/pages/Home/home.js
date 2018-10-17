28526b911247a1512a4451b1e6e8d5990a12373e
Malak55 <38335796+Malak55@users.noreply.github.com>
2018-10-17 18:57:26 -0500

    For push

--- client/src/pages/Home/home.js
+++ client/src/pages/Home/home.js
@@ -17,12 +17,14 @@ class Home extends Component{
           // <p>{this.state.test.data}</p>
         <div className="columnDiv">
           <div className="welcomeDiv">
-            <p>Welcome, Conscript! Sign in or Sign up and build your 40K Roster.</p>
-            <div className="welSI">
-              <a class="welcomeSI" href="/signin">Sign In</a>
-            </div>
-            <div className="welSU">
-              <a class="welcomeSU" href="/signup">Sign Up</a>
+            <p className="welcomeFont">Welcome, Conscript! Sign in or Sign up and build your 40K Roster.</p>
+            <div className="welcomeInput">
+              <div className="welSI">
+                <a class="welcomeSI" href="/signin">Sign In</a>
+              </div>
+              <div className="welSU">
+                <a class="welcomeSU" href="/signup">Sign Up</a>
+              </div>
             </div>
           </div>
         </div>
